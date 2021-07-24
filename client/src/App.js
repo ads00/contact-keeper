9e1b79575881dcfc2927a745edf7968a3fb74213
ads00 <adshayanbalachandran@gmail.com>
2021-07-25 00:21:44 +0530

    Transitions added for contact display

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { Fragment, useContext } from 'react';
+import { Fragment } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
